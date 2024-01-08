d963b61dbd3eb60a8c8a7d650f16547b04f84f06
Coding-noob-helpMePlz <67637342+Coding-noob-helpMePlz@users.noreply.github.com>
2024-01-09 00:29:20 +0800

    chore: delete useless comments

--- src/constants/futureCalendar.constant.ts
+++ src/constants/futureCalendar.constant.ts
@@ -1,6 +1,4 @@
 export const EXPIRY_DAYS_RANGE_T_PLUS_29 = 29; //which means the expiry range is today + next 29 days, total 30 days
 export const EXPIRY_DAYS_RANGE_T_PLUS_30 = 30; //which means the expiry range is today + next 30 days, total 31 days
-
-
-export const MAX_PREVIOUS_MONTHS = 2; // e.g., 6 months back
-export const MAX_FUTURE_MONTHS = 3; // e.g., 12 months ahead
+export const MAX_PREVIOUS_MONTHS = 2;
+export const MAX_FUTURE_MONTHS = 3;
